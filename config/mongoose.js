1a654bf615b1366c9ae62cdf3012df38c10ee41b
Hana Um <hanau12@gmail.com>
2015-10-27 21:54:15 -0700

    Update mongo path try 3

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var fs = require('fs');
-mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://jkhh:jkhh123@ds035553.mongolab.com:35553/mocproject');
 var models_path = __dirname + '/../server/models';
 //mongodb://jkhh:jkhh123@ds035553.mongolab.com:35553/mocproject
 fs.readdirSync(models_path).forEach( function (file) {
