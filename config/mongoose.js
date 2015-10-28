b16848eb66d2556c1c5583e7f9e573a2b224b45d
Hana Um <hanau12@gmail.com>
2015-10-27 21:47:44 -0700

    Updated mongo path

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var fs = require('fs');
-mongoose.connect('mongodb://jkhh:jkhh123@ds035553.mongolab.com:35553/mocproject');
+mongoose.connect('mongodb://heroku_0v4q6hdk:nhht2iodbhje8atpiqh0vr34fn@ds045644.mongolab.com:45644/heroku_0v4q6hdk');
 var models_path = __dirname + '/../server/models';
 //mongodb://jkhh:jkhh123@ds035553.mongolab.com:35553/mocproject
 fs.readdirSync(models_path).forEach( function (file) {
